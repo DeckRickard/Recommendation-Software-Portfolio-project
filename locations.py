130f205091cb0296957e8f9d7adced123c85adc8
Rowan <rowjw97@gmail.com>
2023-06-11 12:15:57 +0100

    Initial commit of basic location data.

---
+++ locations.py
@@ -0,0 +1,47 @@
+locations = {
+    "Topkapi Palace": 
+    {"type": "attraction",
+      "location": "Fatih",
+      "tags": ("history", "museum", "palace", "ottoman")
+      },
+    "Hagia Sophia":
+    {"type": "attraction",
+     "location": "Fatih",
+     "tags": ("history", "byzantine", "ottoman", "museum", "religion")
+     },
+     "Blue Mosque":
+    {"type": "attraction",
+     "location": "Fatih",
+     "tags": ("history", "ottoman", "museum", "religion")
+     },
+     "Grand Bazaar":
+    {"type": "market",
+     "location": "Fatih",
+     "tags": ("shopping", "history", "ottoman")
+     },
+     "1453 Panorama History Museum":
+    {"type": "attraction",
+     "location": "Zeytinburnu",
+     "tags": ("history", "ruins", "ottoman")
+     },
+     "Galata Tower":
+    {"type": "attraction",
+     "location": "Beyoglu",
+     "tags": ("history", "view", "ottoman", "popular")
+     },
+     "Maiden's Tower":
+    {"type": "attraction",
+     "location": "Uskudar",
+     "tags": ("history", "view", "ottoman", "popular")
+     },
+     "Ciya Sofrasi":
+    {"type": "food",
+     "location": "Kadikoy",
+     "tags": ("dinner", "turkish", "traditional")
+     },
+    "Hafiz Mustafa 1864":
+    {"type": "food",
+     "location": "Fatih",
+     "tags": ("dessert", "turkish", "traditional", "ottoman")
+     },
+}
