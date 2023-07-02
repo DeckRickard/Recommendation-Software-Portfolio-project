f875609ae1106df58081669806b705ef8b84b3ff
Rowan <rowjw97@gmail.com>
2023-07-02 15:47:08 +0100

    Expanded possible list of locations.

--- locations.py
+++ locations.py
@@ -44,4 +44,34 @@ locations = {
      "location": "Fatih",
      "tags": ("Dessert", "Turkish", "Traditional", "Ottoman")
      },
+     "Rumeli Fortress":
+    {"type": "Attraction",
+     "location": "Besiktas",
+     "tags": ("Ruins", "Historical", "Ottoman")
+     },    
+      "Spice Bazaar":
+    {"type": "Attraction",
+     "location": "Fatih",
+     "tags": ("Ruins", "Historical", "Ottoman")
+     },    
+      "Basilica Cisterm":
+    {"type": "Attraction",
+     "location": "Fatih",
+     "tags": ("Ruins", "Historical", "Byzantine")
+     },          
+      "Istiklal Street":
+    {"type": "Attraction",
+     "location": "Beyoglu",
+     "tags": ("Lively", "Shopping", "Popular")
+     },   
+      "Istanbul Archaeological Museums":
+    {"type": "Museum",
+     "location": "Fatih",
+     "tags": ("Archeaology", "Historical", "Ottoman", "Byzantine", "Ruins")
+     },   
+      "Turkish and Islamic Arts Museum":
+    {"type": "Museum",
+     "location": "Fatih",
+     "tags": ("Art", "Historical", "Ottoman", "Religion")
+     },   
 }
